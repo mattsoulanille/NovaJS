7e0f8f72c743f88833d62f1d8775a8ad8bdadb62
Matthew Soulanille <matthew@soulanille.net>
2016-07-06 20:40:27 -0700

    changed medium blaster count from 500 to 5 on test ship

--- Nova/nova.js
+++ Nova/nova.js
@@ -108,7 +108,7 @@ playerShip.prototype.updateStats = function() {
 
 var spaceObjects = []
 //var myShip = new playerShip("Starbridge A")
-var medium_blaster = new outfit("Medium Blaster", 500)
+var medium_blaster = new outfit("Medium Blaster", 5)
 var myShip = new playerShip("Starbridge A", [medium_blaster])
 var starbridge = new ship("Starbridge A")
 var shuttle = new ship("Shuttle A")
