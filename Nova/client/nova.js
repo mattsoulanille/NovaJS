9c299fc3a59d448101c2294b10409c9a4b309c66
Matthew Soulanille <matthew@soulanille.net>
2016-07-29 14:07:21 -0700

    clients no longer send a bajillion keypress updates when they hold keys down

--- Nova/client/nova.js
+++ Nova/client/nova.js
@@ -31,6 +31,7 @@ var fullscreen = function() {
 
 
 var socket = io();
+var last_keys = KeyboardJS.activeKeys();
 document.onkeydown = function(e) {
     e = e || event;
     blocked_keys = [37, 38, 39, 40, 32, 9, 17];
@@ -46,11 +47,14 @@ document.onkeydown = function(e) {
 	fullscreen();
     }
     
-    
-    var newStats = {};
-    newStats[myShip.UUID] = myShip.getStats();
-    socket.emit('updateStats', newStats)
-    
+    new_keys = KeyboardJS.activeKeys();
+    if (!new_keys.equals(last_keys)) {
+	var newStats = {};
+	newStats[myShip.UUID] = myShip.getStats();
+	socket.emit('updateStats', newStats);
+
+    }
+    last_keys = KeyboardJS.activeKeys();    
     if (_.contains(blocked_keys, e.keyCode)) {
 	return false;
     }
@@ -59,6 +63,7 @@ document.onkeydown = function(e) {
     }
 }
 document.onkeyup = function(e) {
+    last_keys = KeyboardJS.activeKeys();
     myShip.updateStats();
     var newStats = {};
     newStats[myShip.UUID] = myShip.getStats();
@@ -284,3 +289,36 @@ function onResize() {
     stars.resize()
     myShip.statusBar.resize()
 }
+
+
+
+
+if(Array.prototype.equals) {
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+}
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+	return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+	return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+	// Check if we have nested arrays
+	if (this[i] instanceof Array && array[i] instanceof Array) {
+	    // recurse into the nested arrays
+	    if (!this[i].equals(array[i]))
+		return false;
+	}
+	else if (this[i] != array[i]) {
+	    // Warning - two different object instances will never be equal: {x:20} != {x:20}
+	    return false;
+	}
+    }
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
