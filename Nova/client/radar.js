01cc3fac2d2342b3f84a4136279356dce37e3e28
Matthew Soulanille <matthew@soulanille.net>
2016-07-31 18:01:31 -0700

    non-player ships are now gray on radar

--- Nova/client/radar.js
+++ Nova/client/radar.js
@@ -9,20 +9,33 @@ function radar(meta, source, iff, density) {
     this.density = density || false;
 }
 
-radar.prototype.scale = [2000,2000]; // scale is the dimensions of the radar in nova coords.
+radar.prototype.scale = [6000,6000]; // scale is the dimensions of the radar in nova coords.
 
 radar.prototype.render = function() {
     this.graphics.clear();
     
-    //this.drawDot([0,0], 0xFFFFFF);
-    _.each(this.system.ships, function(s) {
-	this.drawShip(s);
-    }.bind(this));
+    this.drawDot(this.source.position, 0xFFFFFF);
+    for (var i = 1; i < this.system.ships.length; i++) {
+	this.drawShip(this.system.ships[i]);
+    }
 }
 
 
 radar.prototype.drawShip = function(s) {
-    this.drawDot(s.position, 0xFFFFFF);
+    var color;
+    var size;
+    if (this.iff) {
+	color = 0x0000FF; // implement whether they hate you later
+	// color = 0xFF0000 // angry
+
+    }
+    else {
+	color = this.meta.colors.dimRadar;
+    }
+    this.drawDot(s.position, color);
+}
+radar.prototype.drawPlanet = function(p) {
+    // color = 0xFFFF00 // neutral
 }
 
 radar.prototype.drawDot = function(pos, color) {
