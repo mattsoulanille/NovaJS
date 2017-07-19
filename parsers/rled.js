10b30a56f2ece418e58e97a944d177b3c9b67684
Paul Soulanille <paul@soulanille.net>
2017-07-18 22:06:14 -0700

    parses frames now, outputs pngs with transparency in array <name>.frames

--- parsers/rled.js
+++ parsers/rled.js
@@ -1,4 +1,6 @@
 "use strict";
+var PNG = require("pngjs").PNG;
+var fs = require("fs");
 
 // see https://github.com/tjhancocks/OpenNova/blob/master/ResourceKit/ResourceFork/Parsers/RKRLEResourceParser.m
 // see https://github.com/tjhancocks/OpenNova/blob/master/ResourceKit/Categories/NSData%2BParsing.h
@@ -24,82 +26,146 @@ var rled = function(resource) {
     RLEOpCode_PixelRun = 0x04;
     */
 
-    var mapColor = function(color){
+    var mapSetColor = function(place,offset,color){
 
+	var blue = color & 0x001F;//5 bits
+	var green = (color & 0x03E0) >> 5;//5 bits
+	var red = (color & 0x7C00) >> 10;//5 bits
+	var alpha = 0xFF;// * ((color & 0x8000) >> 15);
+	
+	//scale
+	blue = blue << 3;
+	green = green << 3;
+	red = red << 3;
 
-	return color;
+	//refit
+	blue |= blue >> 5;
+	green |= green >> 5;
+	red |= red >> 5;
+
+	//avoid sign bit annoyance cause matt wants it positive, less efficient but doesn't matter after conversion to image
+//	var rgb = (red << 16) | (green << 8) | blue;
+
+//	console.log(green);
+//	console.log(red);
+	place.data[offset + 0] = 0xFF & red;
+	place.data[offset + 1] = 0xFF & green;
+	place.data[offset + 2] = 0xFF & blue;
+	place.data[offset + 3] = 0xFF & alpha;
+	
+	//	return rgb + (alpha * 0x01000000);
 
     }
     
-    var pointer = 16;
-    var frame = 0;
-    var line = 0;
+    var pointer = 16;//_data.position
+    var position = 0;
+    var rowStart = 0;
+    var currentLine = -1;
+    //var currentOffset = 0; for the storage, unneeded here
     var col = 0;
+    
+    var opcode = 0;
+    var count = 0;
     var pixel = 0;
-
+    var currentFrame = 0;
+    var pixelRun = 0;
     
     out.frames = []; //will change when matt gives better format
+    out.frames[0] = new PNG({filterType:4,
+			     width: out.size[0],
+			     height: out.size[1]});
 
-    var done = false;
+
+
     
-    while( !done ) { //rled has an opcode which says the end
-	var opcode = d.getUint8(pointer);
-	var args = d.getUint32(pointer) - opcode * 0x01000000;
-	pointer += 4;
-	switch (opcode) {
-	case 0://RLEOpCode_EndOfFrame = 0x00; 
-	    if (line != out.size[1]-1) 
-		throw "wrong number of lines in frame!";
-	    frame++;
-	    if (frame >= out.numberOfFrames){
-		done = true;
-	    }else{
-		line = 0;
+    var lineLength = out.size[0];
+    
+    !function() {
+	while( true ) { //rled has an opcode which says the end
+	    position = pointer;
+	    
+	    if ((rowStart != 0) && ((position - rowStart) & 0x03)) {
+		position += 4 - ((position - rowStart) & 0x03);
+		pointer += 4 - (count & 0x03);
+	    }
+	    
+	    
+	    count = d.getUint32(pointer); pointer += 4;
+	    opcode = (count & 0xFF000000) >> 24;
+	    count &= 0x00FFFFFF;
+	    
+	    
+	    switch (opcode) {
+	    case 0://RLEOpCode_EndOfFrame = 0x00; 
+		if (currentLine != out.size[1]-1) 
+		    throw "wrong number of lines in frame!:" + currentLine+"≠"+(out.size[1]-1);
+		if (++currentFrame >= out.numberOfFrames)
+		    return;
+		 
+		currentLine = -1;
+
+		out.frames[currentFrame] = new PNG({filterType:4,
+						    width: out.size[0],
+						    height: out.size[1]});
+	   
+		break;
+	    case 1://RLEOpCode_LineStart = 0x01; 
+		
+		++currentLine;
 		col = 0;
-		out.frames[frame] = new Array(out.size[0]);
-	    }
-	    break;
-	case 1://RLEOpCode_LineStart = 0x01; 
-	    out.frames[frame][line] = new Array(out.size[1]).fill(0);
-	    line++;
-	    col = 0;
-	    break;
-	case 2://RLEOpCode_PixelData = 0x02;
-	    for(var i = 0; i < args; i+= 2){
+		
+		rowStart = pointer;
 
-		pixel = d.getUint16(pointer)
-		pointer += 2;
+//		out.frames[currentFrame][currentLine] = new Array(lineLength).fill(0); //default is clear
 		
-		out.frames[frame][line][col] = mapColor(pixel);
+		break;
+	    case 2://RLEOpCode_PixelData = 0x02;
+		for(var i = 0; i < count; i+= 2){
+		    
+		    pixel = d.getUint16(pointer); pointer += 2;
+
+		    var offset = (currentLine * out.size[0] + col) << 2;
+		    mapSetColor( out.frames[currentFrame] , offset , pixel); col ++;
+		    
+
+		}
+
+		if (count & 0x03)
+		    pointer += 4-(count & 0x03);//realign
 		
-		col++;
+		
+		break;
+	    case 3://RLEOpCode_TransparentRun = 0x03;
+		
+		col += (count >> ((out.bytesPerPixel >> 3) - 1));
+		break;
+	    case 4://RLEOpCode_PixelRun = 0x04;
+		pixelRun = d.getUint32(pointer); pointer += 4;
+
+		for (var i = 0; i < count ; i += 4){
+		    var offset = (currentLine * out.size[0] + col) << 2;
+		    mapSetColor( out.frames[currentFrame] , offset , pixel); col ++;
+		    if (i+2 < count){
+			var offset = (currentLine * out.size[0] + col) << 2;
+			mapSetColor( out.frames[currentFrame] , offset , pixel); col ++;
+		    } // allignment
+		    
+		}
+		break;
 	    }
 	    
-	    pointer += 4-(args & 0x03);//realign
-
 	    
-	    break;
-	case 3://RLEOpCode_TransparentRun = 0x03;
-
-	    col += (args >> ((out.bytesPerPixel >> 3) - 1));
-	    break;
-	case 4://RLEOpCode_PixelRun = 0x04;
-	    var pixelrun = d.getUint32(pointer);
-	    pointer += 4;
-	    for (var i = 0; i < args ; i += 4){
-		out.frames[frame][line][col] = mapColor(pixel);
-		col++;
-		if (i+2 < args){
-		    out.frames[frame][line][col] = mapColor(pixel);
-		    col++;
-		} // allignment
-
-	    }
-	    break;
 	}
-	
-	
-    }
+    }();
+    //    console.log(out.frames);
+
+//    mapSetColor( out.frames[0] , 0 , 0x07e0);//grn test
+    
+//    out.frames[0].pack().pipe(fs.createWriteStream("./test/files/rleds/frame0.png")
+//			      .on('finish', function() {
+//				  return out;
+// 			      }));
+
 
     return out;
 };
