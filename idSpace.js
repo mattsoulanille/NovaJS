c1f96709955a2b87df01ef3a395c672977a8d8a2
Matthew Soulanille <matthew@soulanille.net>
2017-07-20 22:20:12 -0700

    made a system for keeping track of ids across plugins

---
+++ idSpace.js
@@ -0,0 +1,97 @@
+"use strict";
+
+
+
+var idSpace = class {
+    constructor(global) {
+	this.spaces = {};
+	// all the resources in existance
+	// stored by type first. then by id (ex: resources['weap']['nova:128'])
+	this._resources = {};
+
+	// kind of like defaultDict from python
+	this.resources = new Proxy(this._resources, {
+	    // this should probably be replaced with just adding each nova type to the object manually.
+	    get: function(target, property, receiver) {
+		if (!(property in target) ) {
+		    //use in to also check the prototype chain
+		    target[property] = {};
+		}
+		return target[property];
+	    }
+	});
+
+
+
+	// add global nova resources to resources
+	this.addPlugin(global, "nova");
+    }
+
+    addPlugin(resources, prefix) {
+
+	var pluginSpace = this.getSpace(prefix);
+	Object.keys(resources).forEach(function(type) {
+	    Object.keys(resources[type]).forEach(function(id) {
+		// remember that novaSpace is a proxy.
+		pluginSpace[type][id] = resources[type][id];
+	    });
+	});
+    }
+    
+    getSpace(prefix) {
+	// opens an id space with the given prefix
+	if (! this.spaces.hasOwnProperty(prefix)) {
+	    var p = new Proxy({}, {
+		get: function(target, property, receiver) {
+		    if (typeof property === "symbol") {
+			return false;
+		    }
+		    if (!(property in target)) {
+			target[property] = new Proxy(this.resources[property], {
+			    get: function(target, id, receiver) {
+				if (typeof id === "symbol") {
+				    return false;
+				}
+				
+				if ( ("nova:" + String(id)) in target ) {
+				    return target["nova:" + String(id)];
+				}
+
+				return target[prefix + ":" + String(id)];
+			    },
+			    set: function(target, id, value, receiver) {
+				if (typeof id === "symbol") {
+				    return true;
+				}
+
+				if ( ("nova:" + String(id)) in target ) {
+				    target["nova:" + String(id)] = value;
+				    return true;
+				}
+
+				target[prefix + ":" + String(id)] = value;
+				return true;
+			    }
+			});
+		    }
+		    return target[property];
+
+		}.bind(this),
+		set: function() {
+		    throw "tried to set entire type of resoures";
+		}
+	    });
+
+	    this.spaces[prefix] = p;
+
+	}
+
+	return this.spaces[prefix];
+
+    }
+
+
+
+};
+
+module.exports = idSpace;
