8f440a9a9cb6163cd721cbd796dbaa61c2313df0
Matthew Soulanille <matthew@soulanille.net>
2017-07-22 16:34:47 -0700

    new tests for rled

--- test/rledTest.js
+++ test/rledTest.js
@@ -27,13 +27,44 @@ describe("rled", function() {
 	});	
     };
 
+    let getFrames = function(png, dim) {
+
+
+	// dim is dim of each frame;
+	assert.equal(png.height % dim.height, 0);
+	assert.equal(png.width % dim.width, 0);
+
+	var out = [];
+
+	for (var y = 0; y < png.height; y += dim.height) {
+	    for (var x = 0; x < png.width; x += dim.width) {
+		var outPNG = new PNG({filtertype:4, width:dim.width, height:dim.height});
+
+		for (var xi = 0; xi < dim.width; xi++) {
+		    for (var yi = 0; yi < dim.height; yi++) {
+			var outidx = (outPNG.width * yi + xi) << 2;
+			var sourceidx = (png.width * (y + yi) + x + xi) << 2;
+			outPNG.data[outidx] = png.data[sourceidx];
+			outPNG.data[outidx+1] = png.data[sourceidx+1];
+			outPNG.data[outidx+2] = png.data[sourceidx+2];
+			outPNG.data[outidx+3] = png.data[sourceidx+3];
+		    }
+		}
+
+		out.push(outPNG);
+	    }
+	}
+	return out;
+
+    };
+    
     let comparePNGs = function(png1, png2) {
 	assert(png1 instanceof PNG);
 	assert(png2 instanceof PNG);
 	assert.equal(png1.width, png2.width);
 	assert.equal(png1.height, png2.height);
 	assert.equal(png1.gamma, png2.gamma);
-	assert(png1.buffer.equals(png2.buffer));
+	assert(png1.data.equals(png2.data));
     };
 
     let applyMask = function(image, mask) {
@@ -106,13 +137,66 @@ describe("rled", function() {
 	comparePNGs(leviathan.mask, leviathanMask);
     });
     */
-    it("should combine the image and mask to make a new image with alpha", function() {
-	var starbridge_masked = applyMask(starbridgePNG, starbridgeMask);
-	var leviathan_masked = applyMask(leviathanPNG, leviathanMask);
 
-	comparePNGs(starbridge.image, starbridge_masked);
-	comparePNGs(leviathan.image, leviathan_masked);
+    it("comparePNGs should work", function() {
+	comparePNGs(starbridgePNG, starbridgePNG);
+    });
+
+    it("getFrames should work", async function() {
+
+	var starbridgeFrames = getFrames(starbridgePNG, {width:48, height:48});
+
+	var pngs = [];
+	var promises = [];
+	for (var i = 0; i < 108; i++) {
+	    var path = "./test/files/rleds/testFrames/" + "starbridge" + i + ".png";
+	    promises.push(async function() {
+		var index = i;
+		pngs[index] = await getPNG(path);
+	    }());
+	}
+
+	await Promise.all(promises);
+
+	for (var i = 0; i < 108; i++) {
+	    comparePNGs(pngs[i], starbridgeFrames[i]);
+	}
 	
+    });
+    
+    it("should produce an ordered array of frames", async function() {
+
+	var starbridgeApplied = applyMask(starbridgePNG, starbridgeMask);
+	var leviathanApplied = applyMask(leviathanPNG, leviathanMask);
+
+	var starbridgeFrames = getFrames(starbridgeApplied, {width:48, height:48});
+	var leviathanFrames = getFrames(leviathanApplied, {width:144, height:144});
+
+	assert.equal(starbridge.frames.length, starbridgeFrames.length);
+	assert.equal(leviathan.frames.length, leviathanFrames.length);
+
+	for (var i = 0; i < starbridge.frames.length; i++) {
+	    comparePNGs(starbridgeFrames[i], starbridge.frames[i]);
+	}
+
+	for (var i = 0; i < leviathan.frames.length; i++) {
+	    comparePNGs(leviathanFrames[i], leviathan.frames[i]);
+	}
+
+
+/*
+	var promises = starbridgeFrames.map(function(image, index) {
+	    return new Promise(function(fulfill, reject) {
+		image.pack().pipe(fs.createWriteStream(")
+				  .on('finish', function() {
+				      fulfill();
+				  }));
+
+	    });
+	});
+
+	await Promise.all(promises);
+*/	
 	/*
 	// cheapo testing of applyMask
 	leviathan_masked.pack().pipe(fs.createWriteStream("./test/files/rleds/testWriting.png")
