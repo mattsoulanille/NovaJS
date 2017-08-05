097752530b6d3c9617e17032d90fad86b5406de2
Matthew Soulanille <matthew@soulanille.net>
2017-08-05 18:36:10 -0400

    more ship tests

--- test/shipTest.js
+++ test/shipTest.js
@@ -36,13 +36,68 @@ describe("ship", function() {
 	expect(s1.cost).to.equal(1);
     });
 
-    it("should parse techLevel", function() {
-	expect(s1.techLevel).to.equal(2);
+    it("should parse deathDelay", function() {
+	expect(s1.deathDelay).to.equal(67);
+    });
+
+    it("should parse deathDelay", function() {
+	expect(s1.deathDelay).to.equal(67);
+    });
+
+    it("should parse initialExplosion", function() {
+	expect(s1.initialExplosion).to.equal(168);
+    });
+
+    it("should parse finalExplosion", function() {
+	expect(s1.finalExplosion).to.equal(169);
+    });
+    
+    it("should parse finalExplosionSparks", function() {
+	expect(s1.finalExplosionSparks).to.equal(true);
     });
 
     it("should parse displayOrder", function() {
 	expect(s1.displayOrder).to.equal(3);
     });
+
+    it("should parse mass", function() {
+	expect(s1.mass).to.equal(6);
+    });
+
+    it("should parse length", function() {
+	expect(s1.length).to.equal(8);
+    });
+
+    it("should parse inherentAI", function() {
+	expect(s1.inherentAI).to.equal(1);
+    });
+
+    it("should parse crew", function() {
+	expect(s1.crew).to.equal(16);
+    });
+
+    it("should parse strength", function() {
+	expect(s1.strength).to.equal(10);
+    });
+
+    it("should parse inherentGovt", function() {
+	expect(s1.inherentGovt).to.equal(128);
+    });
+
+    it("should parse flags", function() {
+	// write me
+	//expect(s1.flags).to.equal(6);
+    });
+
+    it("should parse podCount", function() {
+	// escape pods
+	expect(s1.podCount).to.equal(9);
+    });
+    
+    it("should parse techLevel", function() {
+	expect(s1.techLevel).to.equal(2);
+    });
+
 
     it("should parse buyRandom", function() {
 	expect(s1.buyRandom).to.equal(4);
@@ -96,12 +151,49 @@ describe("ship", function() {
 	expect(s1.energyRecharge).to.equal(22);
     });
 
-    it("should parse ionization", function() {
-	expect(s1.ionization).to.equal(23);
+    it("should parse skillVariation", function() {
+	expect(s1.skillVariation).to.equal(5);
+    });
+
+    it("should parse availabilityNCB", function() {
+	expect(s1.availabilityNCB).to.equal("b13");
+    });
+
+    it("should parse appearOn", function() {
+	expect(s1.appearOn).to.equal("b123 & (b232 | !b42)");
+    });
+
+    it("should parse onPurchase", function() {
+	expect(s1.onPurchase).to.equal("b1 b2 !b3 ^b4");
     });
 
     it("should parse deionize", function() {
 	expect(s1.deionize).to.equal(24);
+    });
+
+    it("should parse ionization", function() {
+	expect(s1.ionization).to.equal(23);
+    });
+
+    it("should parse keyCarried", function() {
+	expect(s1.keyCarried).to.equal(137);
+    });
+
+    // I don't know what defaultItems2 is
+
+    it("should parse outfits", function() {
+	expect(s1.outfits).to.deep.equal(
+	    {
+		149: {'count':50},
+		151: {'count':52},
+		153: {'count':54},
+		155: {'count':56},
+		157: {'count':58},
+		159: {'count':60},
+		161: {'count':62},
+		163: {'count':64}
+	    }
+	);
     });
 
     it("should parse weapons", function() {
@@ -119,20 +211,6 @@ describe("ship", function() {
 	);
     });
 
-    it("should parse outfits", function() {
-	expect(s1.outfits).to.deep.equal(
-	    {
-		149: {'count':50},
-		151: {'count':52},
-		153: {'count':54},
-		155: {'count':56},
-		157: {'count':58},
-		159: {'count':60},
-		161: {'count':62},
-		163: {'count':64}
-	    }
-	);
-    });
 
     it("should parse maxGuns", function() {
 	expect(s1.maxGuns).to.equal(65);
