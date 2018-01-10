854ee565ca3d44f1d1aa3dfb949ca5df9a8c6e62
Paul Soulanille <paul@soulanille.net>
2018-01-09 19:52:33 -0800

    parses everything tested for correctly (note: energy and weapons[4].count tests are wrong for me)

--- parsers/ship.js
+++ parsers/ship.js
@@ -26,7 +26,7 @@ var ship = class extends base {
 
 	//stock weapons
 	this.weapons = [];
-	for (var i = 0; i < 8 ; i ++){
+	for (var i = 0; i < 4 ; i ++){
 	    this.weapons[i] = {};
 	    this.weapons[i].id = d.getInt16(18+2*i);
 	    this.weapons[i].count = d.getInt16(26+2*i);
@@ -38,12 +38,12 @@ var ship = class extends base {
 	this.techLevel = d.getInt16(46);
 	//???
 	this.cost = d.getInt16(50);
-	//weaps is strange
+	//weaps is strange, very strange
 	for (var i = 4; i < 8 ; i ++){
 	    this.weapons[i] = {};
-	    this.weapons[i].id = d.getInt16(42+2*i);
-	    this.weapons[i].count = d.getInt16(50+2*i);
-	    this.weapons[i].ammo = d.getInt16(58+2*i);
+	    this.weapons[i].id = d.getInt16(1742+2*i-8);
+	    this.weapons[i].count = d.getInt16(1750+2*i-8);
+	    this.weapons[i].ammo = d.getInt16(1758+2*i-8);
 	    
 	}
 
@@ -61,37 +61,67 @@ var ship = class extends base {
 	}
 	this.displayOrder = d.getInt16(60);
 
-	this.mass = d.getInt16(60);
-	this.length = d.getInt16(62);
-	this.inherentAI = d.getInt16(64);
-	this.crew = d.getInt16(66);
-	this.strength = d.getInt16(68);
+	this.mass = d.getInt16(62);
+	this.length = d.getInt16(64);
+	this.inherentAI = d.getInt16(66);
+	this.crew = d.getInt16(68);
+	this.strength = d.getInt16(70);
 
-	this.inherentGovt = d.getInt16(70);
-	this.flagsN = d.getInt16(72);
+	this.inherentGovt = d.getInt16(72);
+	this.flagsN = d.getInt16(74);
 
 
 
 	
-	this.podCount = d.getInt16(74);
-	this.defaultItems = [];
-	for (var i = 0; i < 8 ; i ++){
-	    this.defaultItems[i] = {};
-	    this.defaultItems[i].id = d.getInt16(76+2*i);
-	    this.defaultItems[i].count = d.getInt16(84+2*i);
+	this.podCount = d.getInt16(76);
+	this.outfits = [];
+	for (var i = 0; i < 4 ; i ++){
+	    this.outfits[i] = {};
+	    this.outfits[i].id = d.getInt16(78+2*i);
+	    this.outfits[i].count = d.getInt16(86+2*i);
 	}
 
-	this.energyRecharge = d.getInt16(92);
-	this.skillVariance = d.getInt16(94);
-	this.flags2N = d.getInt16(96);
+	this.energyRecharge = d.getInt16(94);
+	this.skillVariation = d.getInt16(96);
+	this.flags2N = d.getInt16(98);
+
+
+	var getString = function(start,length) {
+	    var s = "";
+	    for (var i = start; i < start + length; i++) {
+		if (0 != d.getUint8(i))
+		    s += String.fromCharCode(d.getUint8(i));
+	    }
+	    return s;
+	}
+
 	
-	this.avalibility = d.getInt16(98);
+	this.availabilityNCB = getString(108,255);
 	
-	this.appearOn = d.getInt16(100);
-	this.onPurchase = d.getInt16(102);
-	this.deionize = d.getInt16(104);
-	this.ionization = d.getInt16(106);
-	this.keyCarried = d.getInt16(108);
+	this.appearOn = getString(363,255);
+	this.onPurchase = getString(618,255);
+	this.deionize = d.getInt16(874);
+	this.ionization = d.getInt16(876);
+	this.keyCarried = d.getInt16(878);
+
+	for (var i = 0; i < 4 ; i ++){
+	    this.outfits[i+4] = {};
+	    this.outfits[i+4].id = d.getInt16(880+2*i);
+	    this.outfits[i+4].count = d.getInt16(888+2*i);
+	}
+	this.contribute = [d.getUint32(896),d.getUint32(900)];
+	this.require = [d.getUint32(896),d.getUint32(900)];
+	this.buyRandom = d.getInt16(904);
+	this.hireRandom = d.getInt16(906);
+	
+	this.onCapture = getString(908,255);
+	this.onRetire = getString(1163,255);
+	this.subtitle = getString(1766,64);
+	this.shortName = getString(1486,64);
+	this.commName = getString(1550,32);
+	this.longName = getString(1582,132);
+	
+	this.escourtType = d.getInt16(1829);
 /*
 	this. = d.getInt16(110);
 	this. = d.getInt16(112);
