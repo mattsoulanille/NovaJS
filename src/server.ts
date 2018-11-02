ddff7e4dadcbe78ad952dc90c49bdd2e6914fae8
Matthew Soulanille <matthew@soulanille.net>
2018-11-02 17:47:06 -0400

    Basic TypeScript page working for client and server. (Game doesn't work)

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import * as express from "express";
+import * as http from "http";
+import * as socket from "socket.io";
+
+
+const app = express();
+const httpServer = new http.Server(app);
+const io = socket(httpServer);
+
+const port = 8000; // FIX ME
+
+app.use("/static", express.static(__dirname + "/static"));
+
+app.use("/", function(req: express.Request, res: express.Response) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+httpServer.listen(port, function() {
+    console.log("listening at port " + port);
+});
