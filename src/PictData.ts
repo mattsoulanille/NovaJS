a33607a09af9058a1d8ab0120e4ae29520334016
Matthew Soulanille <matthew@soulanille.net>
2018-11-26 23:59:02 -0500

    Fix a bug where the default png is loaded from the wrong path

--- src/PictData.ts
+++ src/PictData.ts
@@ -5,10 +5,10 @@ import * as path from "path";
 interface PictData extends BaseData {
     PNG: Buffer
 }
-console.log(path.resolve("./"))
+
 const DefaultPictData: PictData = {
     ...DefaultBaseData,
-    PNG: fs.readFileSync(path.join(__dirname, "default.png"))
+    PNG: fs.readFileSync(require.resolve("./default.png"))
 }
 
 export { PictData, DefaultPictData }
