1674200746b298883fad9509ef2a83cc32af1696
Matthew Soulanille <matthew@soulanille.net>
2018-12-02 21:49:41 -0500

    Removed a file from old convex hull

--- test/convex-hull.d.ts
+++
@@ -1,5 +0,0 @@
-// Type definitions for convex-hull
-// Project: convex-hull
-// Definitions by: Matthew Soulanille <https://github.com/mattsoulanille>
-
-export default function ch(points: Array<Array<number>>): Array<Array<number>>;
