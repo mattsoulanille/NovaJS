a6922e2b7ecb4f9cdaade8c63f093fa8cd0d2aeb
Matthew Soulanille <matthew@soulanille.net>
2018-12-03 00:31:01 -0500

    Add a list of global IDs

---
+++ src/NovaIDs.ts
@@ -0,0 +1,18 @@
+
+
+type NovaIDs = {
+    Ship: Array<string>,
+    Outfit: Array<string>,
+    Weapon: Array<string>,
+    Pict: Array<string>,
+    Planet: Array<string>,
+    System: Array<string>,
+    TargetCorners: Array<string>,
+    SpriteSheet: Array<string>,
+    SpriteSheetImage: Array<string>,
+    SpriteSheetFrames: Array<string>,
+    StatusBar: Array<string>,
+    Explosion: Array<string>
+}
+
+export { NovaIDs }
