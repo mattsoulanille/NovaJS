31a3b88340c4f3754dcaa22c4509303123f24009
Matthew Soulanille <matthew@soulanille.net>
2019-07-22 18:39:01 -0700

    Define interface  for communication

---
+++ src/common/Channel.ts
@@ -0,0 +1,25 @@
+import * as t from "io-ts";
+import { AnyEvent } from "ts-events";
+
+const MessageType = t.unknown;
+type MessageType = t.TypeOf<typeof MessageType>;
+
+const MessageWithSourceType = t.type({
+    source: t.string,
+    message: MessageType
+});
+type MessageWithSourceType = t.TypeOf<typeof MessageWithSourceType>;
+
+interface Channel {
+    send(destination: string, message: MessageType): void,
+    broadcast(message: MessageType): void,
+    // Broadcast is necessary so that when communicating
+    // via socket.io, the client doesn't spam the server with
+    // individual messages to each other client.
+    readonly onMessage: AnyEvent<MessageWithSourceType>;
+    readonly onConnect: AnyEvent<string>;
+    readonly onDisconnect: AnyEvent<string>;
+}
+
+
+export { Channel, MessageType, MessageWithSourceType } 
