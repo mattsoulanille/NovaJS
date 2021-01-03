bdac5958e3cc5872a1efa3ee73abb51e8f050573
Matthew Soulanille <matthew@soulanille.net>
2021-01-03 12:49:12 -0500

    Fix a circular reference in set.ts

--- nova/src/ecs/datatypes/set.ts
+++ nova/src/ecs/datatypes/set.ts
@@ -2,12 +2,12 @@ import { isLeft, left, right } from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
 
 
-export function set<A, O>(Value: t.Type<A, O>) {
-    type Value = t.TypeOf<typeof Value>;
+export function set<A, O>(value: t.Type<A, O>) {
+    type Value = t.TypeOf<typeof value>;
     return new t.Type<Set<Value>, Value[]>(
-        `Set(${Value.name})`,
+        `Set(${value.name})`,
         (u): u is Set<Value> => u instanceof Set
-            && [...u].map(u => Value.is(u)).reduce((a, b) => a && b),
+            && [...u].map(u => value.is(u)).reduce((a, b) => a && b),
         (i, context) => {
             if (!(i instanceof Array)) {
                 const error: t.ValidationError = {
@@ -18,7 +18,7 @@ export function set<A, O>(Value: t.Type<A, O>) {
                 return left([error]);
             }
 
-            const decoded = t.array(Value).validate(i, context);
+            const decoded = t.array(value).validate(i, context);
             if (isLeft(decoded)) {
                 return decoded;
             }
