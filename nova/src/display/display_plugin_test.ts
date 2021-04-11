d669850fe1d21ab79f4b9d233fc421bf8feac78e
Matthew Soulanille <matthew@soulanille.net>
2021-04-10 22:57:03 -0400

    Enable web testing on chromium

---
+++ nova/src/display/display_plugin_test.ts
@@ -0,0 +1,21 @@
+import { MockGameData } from 'novadatainterface/MockGameData';
+import { World } from 'nova_ecs/world';
+import { GameDataResource } from '../nova_plugin/game_data_resource';
+import { Display } from './display_plugin';
+
+
+describe('display plugin', () => {
+    let world: World
+    let gameData: MockGameData;
+
+    beforeEach(() => {
+        world = new World();
+        gameData = new MockGameData();
+        world.resources.set(GameDataResource, gameData);
+        //world.addPlugin(Display);
+    });
+
+    it('does not add a sprite to the stage if the entity is gone', () => {
+
+    });
+});
