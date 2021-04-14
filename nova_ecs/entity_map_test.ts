0a0d5ac69721380e01326f6ae2138542fc8f532c
Matthew Soulanille <matthew@soulanille.net>
2021-04-13 23:21:07 -0400

    New tests for entity_map

--- nova_ecs/entity_map_test.ts
+++ nova_ecs/entity_map_test.ts
@@ -1,36 +1,62 @@
-import produce from "immer";
 import "jasmine";
 import { Component } from "./component";
-import { EntityBuilder } from "./entity";
+import { Entity, EntityBuilder } from "./entity";
 import { EntityMapWrapped } from './entity_map';
 
+const FooComponent = new Component<{ x: number }>('foo');
 const BarComponent = new Component<{ y: string }>('bar');
 
 describe('entity map', () => {
-    it('keeps subscriptions between drafts', () => {
-        const wrapped = new EntityMapWrapped();
-        const setEvents: [string, string?][] = [];
-        wrapped.events.set.subscribe(([uuid, entity]) => {
-            setEvents.push([uuid, entity.components.get(BarComponent)?.y]);
+    it('emits when a component is added to an entity', async () => {
+        const entityMap = new EntityMapWrapped();
+        const changed = new Promise<[string, Entity]>((fulfill) => {
+            entityMap.events.change.subscribe(fulfill);
         });
-        wrapped.set('entity', new EntityBuilder()
+
+        const entity = new EntityBuilder().build();
+        entityMap.set('testEntity', entity);
+
+        entity.components.set(BarComponent, { y: 'hello' });
+
+        await expectAsync(changed).toBeResolvedTo(['testEntity', entity]);
+    });
+
+    it('emits when a component is removed from an entity', async () => {
+        const entityMap = new EntityMapWrapped();
+        const changed = new Promise<[string, Entity]>((fulfill) => {
+            entityMap.events.change.subscribe(fulfill);
+        });
+
+        const entity = new EntityBuilder()
             .addComponent(BarComponent, { y: 'hello' })
-            .build());
+            .build();
+        entityMap.set('testEntity', entity);
 
-        const wrapped2 = produce(wrapped, draft => {
-            draft.set('entity', new EntityBuilder()
-                .addComponent(BarComponent, { y: 'bye' }));
+        entity.components.delete(BarComponent);
+
+        await expectAsync(changed).toBeResolvedTo(['testEntity', entity]);
+    });
+
+    it('does not emit when a component is replaced', async () => {
+        const entityMap = new EntityMapWrapped();
+        let called = false;
+        const changed = new Promise<[string, Entity]>((fulfill) => {
+            entityMap.events.change.subscribe(v => {
+                called = true;
+                fulfill(v);
+            });
         });
 
-        expect(wrapped.get('entity')?.components.get(BarComponent)?.y)
-            .toEqual('hello');
-        expect(wrapped2.get('entity')?.components.get(BarComponent)?.y)
-            .toEqual('bye');
+        const entity = new EntityBuilder()
+            .addComponent(BarComponent, { y: 'hello' })
+            .build();
+        entityMap.set('testEntity', entity);
 
-        expect(setEvents).toEqual([
-            ['entity', 'hello'],
-            ['entity', 'bye']
-        ]);
+        entity.components.set(BarComponent, { y: 'bye' });
+        expect(called).toBeFalse();
+        entity.components.set(FooComponent, { x: 123 });
+        expect(called).toBeTrue();
+
+        await expectAsync(changed).toBeResolvedTo(['testEntity', entity]);
     });
 });
-
