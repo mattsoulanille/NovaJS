09ae35a101a9b389b7ef7f3413918e2ad81b4af7
Matthew Soulanille <matthew@soulanille.net>
2021-06-13 20:14:43 -0400

    Try using a shader for the starfield

--- nova/src/display/starfield_plugin.ts
+++ nova/src/display/starfield_plugin.ts
@@ -12,6 +12,7 @@ import { PixiAppResource } from "./pixi_app_resource";
 import { BOUNDARY } from "nova_ecs/datatypes/position";
 import { Space } from "./space_resource";
 import { PlayerShipSelector } from "../nova_plugin/player_ship_plugin";
+import { ResizeEvent } from "./resize_event";
 
 const STAR_ID = "nova:700";
 
@@ -29,36 +30,88 @@ class Starfield {
     private density: number;
     private positionFactorRange: [number, number];
     container = new PIXI.Container();
-    private layerSprites: PIXI.Sprite[] = [];
-    private placeholderText = new PIXI.Text('This is a starfield');
+    //private layerSprites: PIXI.Sprite[] = [];
+    //private placeholderText = new PIXI.Text('This is a starfield');
+    private graphics = new PIXI.Graphics();
+    private shaderCode = `
+    precision mediump float;
+    uniform vec2 pos;
+    uniform sampler2D star;
 
-    constructor({ textures, renderer, layers, density, positionFactorRange }: StarfieldArgs) {
+    float modu(float a, float b) {
+        return (b * fract(a / b));
+    }
+    float heck(float x, float y) {
+        return fract(sin(x*1742.3231)*1313.0842+cos(y*179.48222)*18321.2);
+    }
+    void main() {
+
+        //gl_FragColor = vec4(gl_FragCoord.x / 1000.0, 0.0, 0.0, 1.0);
+        //gl_FragColor = vec4(pos.x / 1000.0, pos.y / 1000.0, 0.05, 1.0);
+        float posx = gl_FragCoord.x + pos.x*0.5;
+        float posy = gl_FragCoord.y - pos.y*0.5;
+    
+        float u = floor(modu(posx, 50.0))+0.5;
+        float v = floor(modu(posy, 50.0))+0.5;
+        
+        float starcox = floor(posx/50.0);
+        float starcoy = floor(posy/50.0);
+        
+        //float u = gl_FragCoord.x + pos.x;
+        //float v = gl_FragCoord.y + pos.y;
+        float starx = floor(heck(starcox,starcoy)*10.0);
+        float stary = floor(heck(starcox+0.5,starcoy+0.5)*2.0);
+        float nu = starx*5.0+clamp(u,0.0,5.0);
+        float nv = stary*5.0+clamp(v,0.0,5.0);
+        float inStar = step(0.0,u)*step(0.0,5.0-u)*step(0.0,v)*step(0.0,5.0-v);
+        vec2 relPos = vec2(nu/50.0, nv/10.0);
+        gl_FragColor = texture2D(star,  relPos)*inStar;
+    }
+    `;
+    private uniforms: Record<string, any>;
+    private filter: PIXI.Filter;
+
+    constructor({ textures, layers, density, positionFactorRange }: StarfieldArgs) {
         this.textures = textures;
+        this.uniforms = {
+            star: this.textures[0],
+            pos: new PIXI.Point(0, 0),
+        };
+        this.filter = new PIXI.Filter('', this.shaderCode, this.uniforms);
         this.layers = layers;
         this.density = density;
         this.positionFactorRange = positionFactorRange;
-
-        const starSprites = textures.map(starTexture => new PIXI.Sprite(starTexture));
+        this.container.filters = [this.filter];
+        this.container.addChild(this.graphics);
+        //const starSprites = textures.map(starTexture => new PIXI.Sprite(starTexture));
 
 
         // Example starfield with none of the cool features
         // TODO: Issue #102
-        const baseRenderTexture = new PIXI.BaseRenderTexture({ width: 1000, height: 1000 });
-        const renderTexture = new PIXI.RenderTexture(baseRenderTexture);
-        for (let i = 0; i < 10000; i++) {
-            const starSprite = starSprites[Math.floor(Math.random() * starSprites.length)];
-            starSprite.position.x = Math.random() * 1000;
-            starSprite.position.y = Math.random() * 1000;
-            renderer.render(starSprite, { renderTexture, clear: false });
-        }
+        // const baseRenderTexture = new PIXI.BaseRenderTexture({ width: 1000, height: 1000 });
+        // const renderTexture = new PIXI.RenderTexture(baseRenderTexture);
+        // for (let i = 0; i < 10000; i++) {
+        //     const starSprite = starSprites[Math.floor(Math.random() * starSprites.length)];
+        //     starSprite.position.x = Math.random() * 1000;
+        //     starSprite.position.y = Math.random() * 1000;
+        //     renderer.render(starSprite, { renderTexture, clear: false });
+        // }
 
-        const firstLayer = new PIXI.Sprite(renderTexture);
-        this.container.addChild(firstLayer);
-        this.layerSprites = [firstLayer];
+        // const firstLayer = new PIXI.Sprite(renderTexture);
+        // this.container.addChild(firstLayer);
+        // this.layerSprites = [firstLayer];
     }
 
-    draw(_playerPosition: { x: number, y: number }) {
-        // TODO
+    draw(playerPosition: { x: number, y: number }) {
+        this.uniforms.pos.x = playerPosition.x;
+        this.uniforms.pos.y = playerPosition.y;
+    }
+
+    resize(x: number, y: number) {
+        this.graphics.clear();
+        this.graphics.lineStyle(1, 0xffffff);
+        this.graphics.moveTo(0, 0);
+        this.graphics.lineTo(x, y);
     }
 }
 
@@ -76,13 +129,22 @@ export function starfield({ layers = 8, density = 0.00002,
         }
     });
 
+    const StarfieldResize = new System({
+        name: 'StarfieldResize',
+        events: [ResizeEvent],
+        args: [StarfieldResource, ResizeEvent] as const,
+        step(starfield, resize) {
+            starfield.resize(...resize);
+        }
+    });
+
     return {
         name: 'Starfield',
         async build(world) {
-            const stage = world.resources.get(Space);
-            if (!stage) {
-                throw new Error('Expected Stage resource to exist');
-            }
+            // const stage = world.resources.get(Space);
+            // if (!stage) {
+            //     throw new Error('Expected Stage resource to exist');
+            // }
             const gameData = world.resources.get(GameDataResource);
             if (!gameData) {
                 throw new Error('Expected GameData resource to exist');
@@ -91,7 +153,6 @@ export function starfield({ layers = 8, density = 0.00002,
             if (!app) {
                 throw new Error('Expected PixiApp resource to exist');
             }
-
 
             const { frames } = await gameData.data.SpriteSheetFrames.get(STAR_ID);
             const textures = await texturesFromFrames(frames);
@@ -103,9 +164,10 @@ export function starfield({ layers = 8, density = 0.00002,
                 density,
                 positionFactorRange,
             });
-
-            stage.addChild(starfield.container);
+            starfield.resize(app.screen.width, app.screen.height);
+            app.stage.addChild(starfield.container);
             world.resources.set(StarfieldResource, starfield);
+            world.addSystem(StarfieldResize);
             world.addSystem(StarfieldSystem);
         }
     }
