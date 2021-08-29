6f42f4dc33fecb13846e00e38a99dc209fdc6e7c
Matthew Soulanille <matthew@soulanille.net>
2021-08-29 19:47:55 -0400

    Add option to re-run providers when their inputs change

--- nova_ecs/provider.ts
+++ nova_ecs/provider.ts
@@ -1,9 +1,9 @@
 import { left, right } from "fp-ts/lib/Either";
 import { applyPatches, createDraft, enableMapSet, enablePatches, finishDraft, Patch, setAutoFreeze } from "immer";
-import { ArgData, ArgsToData, ArgTypes, GetEntity, UUID } from "./arg_types";
+import { ArgData, ArgsToData, ArgTypes, ComponentsOnly, GetEntity, QueryArgs, UUID } from "./arg_types";
 import { Component, ComponentData, UnknownComponent } from "./component";
 import { DeleteEvent } from "./events";
-import { Modifier } from "./modifier";
+import { AnyModifier, Modifier } from "./modifier";
 import { Optional } from "./optional";
 import { Plugin } from "./plugin";
 import { Query } from "./query";
@@ -11,7 +11,7 @@ import { Resource } from "./resource";
 import { System } from "./system";
 import { DefaultMap } from "./utils";
 
-type ProviderMapEntry = {
+type AsyncProviderMapEntry = {
     promise: Promise<unknown>,
     complete: boolean,
     result?: unknown,
@@ -22,31 +22,58 @@ type ProviderMapEntry = {
 // TODO?: Use a weakmap with entities as keys instead of a map with uuids as keys?
 export class AsyncProviderData {
     providers: DefaultMap<string /* Entity uuid */,
-        Map<UnknownComponent /* Provided symbol */, ProviderMapEntry>>
+        Map<UnknownComponent /* Provided symbol */, AsyncProviderMapEntry>>
         = new DefaultMap(() => new Map());
     done: Promise<void> = Promise.resolve();
 }
 
 export const AsyncProviderResource = new Resource<AsyncProviderData>('AsyncProviderResource')
 
-export function Provide<Provided extends Component<any>, Args extends readonly ArgTypes[]>({ provided, factory, args }: {
-    provided: Provided,
-    factory: (...args: ArgsToData<Args>) => ComponentData<Provided>,
-    args: Args
-}) {
-    return new Modifier({
-        query: new Query([Optional(provided), GetEntity, ...args] as const,
-            `Provide ${provided.name}`),
-        transform: (providedVal, entity, ...factoryArgs) => {
+type ProviderUpdate = DefaultMap<string /* Entity uuid */,
+    Map<AnyModifier /* Provider */, {
+        dependencies: Set<UnknownComponent>,
+        update: boolean,
+        promise?: Promise<void>,
+    }>>;
 
-            if (providedVal) {
+const ProviderUpdateResource = new Resource<ProviderUpdate>('ProviderUpdate');
+
+function getUpdateData(providerUpdate: ProviderUpdate, uuid: string,
+    provider: AnyModifier, update?: Iterable<Component<any>>) {
+    const entityUpdate = providerUpdate.get(uuid);
+    if (!entityUpdate.has(provider)) {
+        entityUpdate.set(provider, {
+            dependencies: new Set([...(update ?? [])]),
+            update: false,
+        });
+    }
+    return entityUpdate.get(provider)!;
+}
+
+export function Provide<Provided extends Component<any>, Args extends readonly ArgTypes[]>({ provided, update, factory, args }: {
+    provided: Provided,
+    update?: Iterable<Component<any>>,
+    factory: (...args: ArgsToData<Args>) => ComponentData<Provided>,
+    args: Args,
+}) {
+    const query = new Query([Optional(provided), GetEntity, UUID,
+        ProviderUpdateResource, ...args] as const, `Provide ${provided.name}`);
+
+    let provider: Modifier<QueryArgs<typeof query>, Provided>;
+    provider = new Modifier({
+        query,
+        transform: (providedVal, entity, uuid, providerUpdate, ...factoryArgs) => {
+            const updateData = getUpdateData(providerUpdate, uuid, provider, update);
+            if (providedVal && !updateData.update) {
                 return right(providedVal as ArgData<Provided>);
             }
             const newVal = factory(...factoryArgs);
             entity.components.set(provided, newVal);
+            updateData.update = false;
             return right(newVal);
         }
     });
+    return provider;
 }
 
 enablePatches();
@@ -57,26 +84,42 @@ setAutoFreeze(false);
 const CheckCacheComponent = new Component<undefined>('CheckCacheComponent');
 
 // TODO: Refactor this with AsyncSystem?
-export function ProvideAsync<Provided extends Component<any>, Args extends readonly ArgTypes[]>({ provided, factory, args }: {
+export function ProvideAsync<Provided extends Component<any>, Args extends readonly ArgTypes[]>({ provided, update, factory, args }: {
     provided: Provided,
+    update?: Iterable<Component<any>>,
     factory: (...args: ArgsToData<Args>) => Promise<ComponentData<Provided>>,
     args: Args
 }) {
     type Data = ComponentData<Provided>;
-    return new Modifier({
-        query: new Query([AsyncProviderResource, Optional(provided), GetEntity,
-            UUID, ...args] as const, `ProvideAsync ${provided.name}`),
-        transform: (providerResource, providedVal, entity, uuid, ...argData) => {
+    const query = new Query([AsyncProviderResource, Optional(provided), GetEntity,
+        UUID, ProviderUpdateResource, ...args] as const, `ProvideAsync ${provided.name}`);
+    let provider: Modifier<QueryArgs<typeof query>, Provided>;
+
+    provider = new Modifier({
+        query,
+        transform: (providerResource, providedVal, entity, uuid, providerUpdate, ...argData) => {
             const providerMap = providerResource.providers.get(uuid)!;
-            if (providedVal) {
+            const updateData = getUpdateData(providerUpdate, uuid, provider, update);
+            const hasNewProvided = providerMap.has(provided as UnknownComponent);
+
+            if (providedVal && !updateData.update && !hasNewProvided) {
                 return right(providedVal as ArgData<Provided>);
-            } else if (providerMap.has(provided as UnknownComponent)) {
+            } else if (hasNewProvided && !updateData.update) {
+                // Side effects in the factory function are supported, but there
+                // is no guarantee that a provided component will appear in the world.
+                // If a dependency changes before the factory completes, the component
+                // will not be added, so be careful when using factory functions that
+                // have side effects that need to be cleaned up by cleanup systems,
+                // since those systems will not run if the component is never in the
+                // world. In this case, it's best to consume the results of the async
+                // provider with a synchronous provider, so only the results that
+                // actually end up being provided have side-effects run on them.
                 const providerMapEntry = providerMap
                     .get(provided as UnknownComponent)!;
 
                 if (providerMapEntry.complete) {
+                    providerMap.delete(provided as UnknownComponent);
                     if (providerMapEntry.error) {
-                        providerMap.delete(provided as UnknownComponent);
                         throw providerMapEntry.error;
                     }
 
@@ -85,9 +128,7 @@ export function ProvideAsync<Provided extends Component<any>, Args extends reado
                     // of the code, but may break in the future.
                     (argData as any)[Symbol.for('immer-state')] = true;
                     applyPatches(argData, providerMapEntry.patches);
-
                     entity.components.set(provided, providerMapEntry.result as Data);
-
                     return right(providerMapEntry.result as ArgData<Provided>);
                 }
                 return left(undefined);
@@ -95,7 +136,7 @@ export function ProvideAsync<Provided extends Component<any>, Args extends reado
                 // TODO: Refactor with Async system
                 const draftArgs = createDraft(argData);
                 const newData = factory(...draftArgs as typeof argData);
-                const providerMapEntry: ProviderMapEntry = {
+                const providerMapEntry: AsyncProviderMapEntry = {
                     complete: false,
                     patches: [],
                     promise: newData.then(result => {
@@ -126,25 +167,45 @@ export function ProvideAsync<Provided extends Component<any>, Args extends reado
                 })();
 
                 providerMap.set(provided as UnknownComponent, providerMapEntry);
+                // Mark that we've re-provided (are re-providing) the result.
+                // Do this here instead of when we return the result so that if
+                // inputs change while it's being generated, the provider will run again.
+                updateData.update = false;
                 return left(undefined);
             }
         }
     });
+    return provider;
 }
 
-const CleanupProviderResourceSystem = new System({
-    name: 'CleanupProviderResourceSystem',
+const CleanupProviderResourcesSystem = new System({
+    name: 'CleanupProviderResourcesSystem',
     events: [DeleteEvent],
-    args: [UUID, AsyncProviderResource] as const,
-    step(uuid, providerResource) {
-        providerResource.providers.delete(uuid);
+    args: [UUID, AsyncProviderResource, ProviderUpdateResource] as const,
+    step(uuid, asyncProviders, providerUpdates) {
+        asyncProviders.providers.delete(uuid);
+        providerUpdates.delete(uuid);
     }
 });
 
-export const ProvideAsyncPlugin: Plugin = {
-    name: 'ProvideAsync',
+export const ProvidePlugin: Plugin = {
+    name: 'ProvidePlugin',
     build: (world) => {
         world.resources.set(AsyncProviderResource, new AsyncProviderData());
-        world.addSystem(CleanupProviderResourceSystem);
+        const providerUpdate: ProviderUpdate
+            = new DefaultMap(() => new Map());
+        world.resources.set(ProviderUpdateResource, providerUpdate);
+        world.entities.events.changeComponent.subscribe(
+            ([uuid, _entity, component]) => {
+                const entityUpdates = providerUpdate.get(uuid);
+
+                for (const providerUpdates of entityUpdates.values()) {
+                    if (!providerUpdates.update) {
+                        // If it's not known to be updating, check it.
+                        providerUpdates.update = providerUpdates.dependencies.has(component);
+                    }
+                }
+            });
+        world.addSystem(CleanupProviderResourcesSystem);
     }
 }
--- nova_ecs/provider_test.ts
+++ nova_ecs/provider_test.ts
@@ -1,5 +1,5 @@
 import 'jasmine';
-import { Component, UnknownComponent } from './component';
+import { Component } from './component';
 import { EntityBuilder } from './entity';
 import { AsyncProviderResource, Provide, ProvideAsync } from './provider';
 import { System } from './system';
@@ -15,18 +15,7 @@ function sleep(ms: number) {
 const FOO_COMPONENT = new Component<{ x: number }>('foo');
 const BAR_COMPONENT = new Component<{ y: string }>('bar');
 
-describe('provider', () => {
-    let clock: jasmine.Clock;
-
-    beforeEach(() => {
-        clock = jasmine.clock();
-        clock.install();
-    });
-
-    afterEach(() => {
-        clock.uninstall();
-    });
-
+describe('Provide', () => {
     it('provides a component if the entity is missing it', () => {
         const world = new World();
 
@@ -92,6 +81,92 @@ describe('provider', () => {
         world.step();
 
         expect(wordLengths).toEqual([['hello', 123]]);
+    });
+
+    it('recreates the provided component when inputs change', () => {
+        const world = new World();
+
+        const fooProvider = Provide({
+            provided: FOO_COMPONENT,
+            args: [BAR_COMPONENT] as const,
+            update: [BAR_COMPONENT],
+            factory: (bar) => {
+                return {
+                    x: bar.y.length
+                }
+            }
+        });
+
+        const wordLengths: [string, number][] = [];
+        const providesFoo = new System({
+            name: 'providesFoo',
+            args: [BAR_COMPONENT, fooProvider] as const,
+            step: (bar, foo) => {
+                wordLengths.push([bar.y, foo.x]);
+            }
+        });
+
+        world.addSystem(providesFoo);
+        const word1 = new EntityBuilder()
+            .addComponent(BAR_COMPONENT, { y: 'hello' }).build();
+
+        world.entities.set('word1', word1);
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5]]);
+
+        word1.components.set(BAR_COMPONENT, { y: 'bye' });
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5], ['bye', 3]]);
+    });
+
+    it('does not recreate the value if not tracking changed input', () => {
+        const world = new World();
+
+        const fooProvider = Provide({
+            provided: FOO_COMPONENT,
+            args: [BAR_COMPONENT] as const,
+            factory: (bar) => {
+                return {
+                    x: bar.y.length
+                }
+            }
+        });
+
+        const wordLengths: [string, number][] = [];
+        const providesFoo = new System({
+            name: 'providesFoo',
+            args: [BAR_COMPONENT, fooProvider] as const,
+            step: (bar, foo) => {
+                wordLengths.push([bar.y, foo.x]);
+            }
+        });
+
+        world.addSystem(providesFoo);
+        const word1 = new EntityBuilder()
+            .addComponent(BAR_COMPONENT, { y: 'hello' }).build();
+
+        world.entities.set('word1', word1);
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5]]);
+
+        word1.components.set(BAR_COMPONENT, { y: 'bye' });
+        world.step();
+
+        // Note 'bye' still has 5 associated with it instead of 3.
+        expect(wordLengths).toEqual([['hello', 5], ['bye', 5]]);
+    });
+});
+
+describe('ProvideAsync', () => {
+    let clock: jasmine.Clock;
+
+    beforeEach(() => {
+        clock = jasmine.clock();
+        clock.install();
+    });
+
+    afterEach(() => {
+        clock.uninstall();
     });
 
     it('provides a component asynchronously', async () => {
@@ -204,5 +279,103 @@ describe('provider', () => {
         world.entities.delete('word1');
         world.step();
         expect(world.resources.get(AsyncProviderResource)?.providers.size).toBe(0);
+    });
+
+    it('recreates the provided component when inputs change', async () => {
+        const world = new World();
+
+        const fooProvider = ProvideAsync({
+            provided: FOO_COMPONENT,
+            args: [BAR_COMPONENT] as const,
+            update: [BAR_COMPONENT],
+            factory: async (bar) => {
+                await sleep(10);
+                return {
+                    x: bar.y.length
+                }
+            }
+        });
+
+        const wordLengths: [string, number][] = [];
+        const providesFoo = new System({
+            name: 'providesFoo',
+            args: [BAR_COMPONENT, fooProvider] as const,
+            step: (bar, foo) => {
+                wordLengths.push([bar.y, foo.x]);
+            }
+        });
+
+        world.addSystem(providesFoo);
+        const word1 = new EntityBuilder()
+            .addComponent(BAR_COMPONENT, { y: 'hello' }).build();
+
+        world.entities.set('word1', word1);
+        world.step();
+        clock.tick(11);
+        await world.resources.get(AsyncProviderResource)?.done;
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5]]);
+
+        word1.components.set(BAR_COMPONENT, { y: 'bye' });
+        world.step();
+        clock.tick(11);
+        await world.resources.get(AsyncProviderResource)?.done;
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5], ['bye', 3]]);
+    });
+
+    it('ignores a provided value if it is out of date', async () => {
+        const world = new World();
+
+        let sleepTime = 20;
+        let foosProvided: number[] = [];
+        const fooProvider = ProvideAsync({
+            provided: FOO_COMPONENT,
+            args: [BAR_COMPONENT] as const,
+            update: [BAR_COMPONENT],
+            factory: async (bar) => {
+                await sleep(sleepTime);
+                foosProvided.push(bar.y.length);
+                return {
+                    x: bar.y.length
+                }
+            }
+        });
+
+        const wordLengths: [string, number][] = [];
+        const providesFoo = new System({
+            name: 'providesFoo',
+            args: [BAR_COMPONENT, fooProvider] as const,
+            step: (bar, foo) => {
+                wordLengths.push([bar.y, foo.x]);
+            }
+        });
+
+        world.addSystem(providesFoo);
+        const word1 = new EntityBuilder()
+            .addComponent(BAR_COMPONENT, { y: 'hello' }).build();
+
+        world.entities.set('word1', word1);
+        world.step();
+        clock.tick(21);
+        await world.resources.get(AsyncProviderResource)?.done;
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5]]);
+
+        // 'bye' will finish after 'bye again', but the value of bar should be
+        // based on 'bye again', i.e. 9.
+        // sleepTime == 20 right now.
+        word1.components.set(BAR_COMPONENT, { y: 'bye' });
+        world.step();
+        sleepTime = 10;
+        word1.components.set(BAR_COMPONENT, { y: 'bye again' });
+        world.step();
+
+        clock.tick(40);
+        await world.resources.get(AsyncProviderResource)?.done;
+        world.step();
+        expect(wordLengths).toEqual([['hello', 5], ['bye again', 9]]);
+        // 'hello' finishes first, then 'bye again', then 'bye'.
+        expect(foosProvided).toEqual([5, 9, 3]);
     });
 });
