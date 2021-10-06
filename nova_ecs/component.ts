82784691ec86aa32fe4e90ae9fc872051f5486be
Matthew Soulanille <matthew@soulanille.net>
2021-10-05 23:11:32 -0700

    Mark component with a unique symbol

--- nova_ecs/component.ts
+++ nova_ecs/component.ts
@@ -2,7 +2,9 @@ import * as t from 'io-ts';
 export type ComponentData<C> = C extends Component<infer Data> ? Data : never;
 export type UnknownComponent = Component<unknown>;
 
+const componentSymbol = Symbol('Component');
 export class Component<Data> {
+    private readonly componentSymbol = componentSymbol;
     readonly type?: t.Type<Data>;
     readonly name: string;
     constructor(name: string) {
