2535a9da29b2955dee44227c6df8d370694da115
Matthew Soulanille <matthew@guppy.home>
2021-10-22 23:27:58 -0700

    Fix beam weapon animation lagging

--- nova/src/nova_plugin/create_time.ts
+++ nova/src/nova_plugin/create_time.ts
@@ -2,11 +2,22 @@ import { Component } from 'nova_ecs/component';
 import { Plugin } from 'nova_ecs/plugin';
 import { TimeResource } from 'nova_ecs/plugins/time_plugin';
 import { Provide } from 'nova_ecs/provider';
+import { ProvideArg } from 'nova_ecs/provide_arg';
 
 
 export const CreateTime = new Component<number>('ProjectileFireTime');
 export const CreateTimeProvider = Provide({
     name: "CreateTimeProvider",
+    provided: CreateTime,
+    args: [TimeResource] as const,
+    factory({ time }) {
+        return time;
+    }
+});
+
+// Used in cases where the creation time is needed on the same frame
+// e.g. BeamSystem.
+export const CreateTimeArgProvider = ProvideArg({
     provided: CreateTime,
     args: [TimeResource] as const,
     factory({ time }) {
