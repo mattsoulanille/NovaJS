2a4f59da511136bc876fdbdb5d78d5beeb5222e9
Matthew Soulanille <matthew@soulanille.net>
2022-05-15 11:59:24 -0400

    Emit damage event instead of calling damage function

--- nova/src/nova_plugin/death_plugin.ts
+++ nova/src/nova_plugin/death_plugin.ts
@@ -1,74 +1,68 @@
 import { WeaponDamage } from 'novadatainterface/WeaponData';
-import { Emit, EmitFunction, RunQuery, RunQueryFunction } from 'nova_ecs/arg_types';
+import { Emit, RunQuery, UUID } from 'nova_ecs/arg_types';
 import { EcsEvent } from 'nova_ecs/events';
 import { Optional } from 'nova_ecs/optional';
 import { Plugin } from 'nova_ecs/plugin';
 import { Time, TimeResource } from 'nova_ecs/plugins/time_plugin';
-import { Query } from 'nova_ecs/query';
-import { Resource } from 'nova_ecs/resource';
+import { System } from 'nova_ecs/system';
 import { ArmorComponent, IonizationColorComponent, IonizationComponent, ShieldComponent } from './health_plugin';
 import { ProjectileComponent } from './projectile_data';
 
-type ApplyDamage = (damage: WeaponDamage, other: string, scale?: number) => void;
-
-const DamageQuery = new Query([Optional(ShieldComponent), Optional(ArmorComponent),
-Optional(IonizationComponent), Optional(IonizationColorComponent),
-Optional(ProjectileComponent), TimeResource] as const);
+// const DamageQuery = new Query([Optional(ShieldComponent), Optional(ArmorComponent),
+// Optional(IonizationComponent), Optional(IonizationColorComponent),
+// Optional(ProjectileComponent), TimeResource] as const);
 
 export const DeathEvent = new EcsEvent<Time>('DeathEvent');
 
-export const ApplyDamageResource = new Resource<ApplyDamage>('ApplyDamageComponent');
+export const DamagedEvent = new EcsEvent<{ damage: WeaponDamage, damager: string, scale?: number }>('DamagedEvent');
 
-function applyDamage(emit: EmitFunction, runQuery: RunQueryFunction,
-    damage: WeaponDamage, other: string, scale = 1) {
-    const result = runQuery(DamageQuery, other)[0];
-    if (!result) {
-        console.warn(`Cannot apply damage to non-existant entity ${other}`);
-        return;
-    }
+const DamageSystem = new System({
+    name: 'DamageSystem',
+    events: [DamagedEvent],
+    args: [Emit, DamagedEvent, Optional(ShieldComponent), Optional(ArmorComponent),
+        Optional(IonizationComponent), Optional(IonizationColorComponent),
+        Optional(ProjectileComponent), TimeResource, UUID] as const,
+    step(emit, { damage, scale = 1 }, shield, armor, ionization, ionizationColor, isProjectile, time, uuid) {
 
-    const [shield, armor, ionization, ionizationColor, otherIsProjectile, time] = result;
-    if (!armor) {
-        return;
-    }
+        const hasShield = shield && shield.max > 0;
+        if (isProjectile && !hasShield) {
+            // This is a projectile, so use point defense damage scaling.
+            damage = {
+                ...damage,
+                armor: damage.armor + damage.shield / 2,
+            };
+        }
 
-    const hasShield = shield && shield.max > 0;
-    if (otherIsProjectile && !hasShield) {
-        // We're colliding with another projectile, so use
-        // point defense damage scaling.
-        damage = {
-            ...damage,
-            armor: damage.armor + damage.shield / 2,
-        };
-    }
+        if (damage.ionization !== 0 && ionization) {
+            ionization.current += damage.ionization * scale;
+            if (ionizationColor) {
+                ionizationColor.color = damage.ionizationColor;
+            }
+        }
 
-    if (damage.ionization !== 0 && ionization) {
-        ionization.current += damage.ionization * scale;
-        if (ionizationColor) {
-            ionizationColor.color = damage.ionizationColor;
+        if (shield && !damage.passThroughShield) {
+            shield.current -= damage.shield * scale;
+            if (shield.current > 0) {
+                return;
+            }
+        }
+        if (armor) {
+            armor.current = Math.max(0, armor.current - damage.armor * scale)
+            if (armor.current === 0) {
+                emit(DeathEvent, time, [uuid]);
+            }
         }
     }
-
-    if (shield && !damage.passThroughShield) {
-        shield.current -= damage.shield * scale;
-        if (shield.current > 0) {
-            return;
-        }
-    }
-
-    armor.current = Math.max(0, armor.current - damage.armor * scale)
-    if (armor.current === 0) {
-        emit(DeathEvent, time, [other]);
-    }
-}
+});
 
 export const DeathPlugin: Plugin = {
     name: 'DeathPlugin',
     build(world) {
-        const runQuery = world.resources.get(RunQuery)!;
-        const emit = world.resources.get(Emit)!;
-        world.resources.set(ApplyDamageResource, (damage, other, scale = 1) => {
-            applyDamage(emit, runQuery, damage, other, scale);
-        });
+        //const runQuery = world.resources.get(RunQuery)!;
+        //const emit = world.resources.get(Emit)!;
+        world.addSystem(DamageSystem);
     },
+    remove(world) {
+        world.removeSystem(DamageSystem);
+    }
 }
