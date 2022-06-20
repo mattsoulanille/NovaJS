994bc7118b5fddc89285d2ee6571c1696c7d5d8e
Matthew Soulanille <matthew@soulanille.net>
2022-06-19 19:34:55 -0700

    Decrease volume (#421)
    * Decrease volume
    
    * Increase volume a bit

--- nova/src/display/sound_plugin.ts
+++ nova/src/display/sound_plugin.ts
@@ -8,18 +8,21 @@ import { GameDataResource } from '../nova_plugin/game_data_resource';
 import { SoundEvent } from '../nova_plugin/sound_event';
 
 const LoopingSounds = new Resource<Map<string, Sound>>('LoopingSounds');
+const VolumeResource = new Resource<{volume: number}>('VolumeResource');
 
 const SoundSystem = new System({
     name: 'SoundSystem',
     events: [SoundEvent],
-    args: [SoundEvent, GameDataResource, LoopingSounds, SingletonComponent] as const,
-    step({ id, loop }, gameData, loopingSounds) {
+    args: [SoundEvent, GameDataResource, LoopingSounds, VolumeResource,
+           SingletonComponent] as const,
+    step({ id, loop }, gameData, loopingSounds, {volume}) {
         if (loop && loopingSounds.has(id)) {
             return;
         }
 
         const maybeSound = (gameData as GameData).data.Sound.getCached(id);
         if (maybeSound) {
+            maybeSound.volume = volume;
             if (loop) {
                 loopingSounds.set(id, maybeSound);
                 maybeSound.play(() => {
@@ -36,10 +39,12 @@ export const SoundPlugin: Plugin = {
     name: 'SoundPlugin',
     build(world) {
         world.resources.set(LoopingSounds, new Map());
+        world.resources.set(VolumeResource, {volume: 0.045});
         world.addSystem(SoundSystem);
     },
     remove(world) {
         world.removeSystem(SoundSystem);
+        world.resources.delete(VolumeResource);
         world.resources.delete(LoopingSounds);
     }
 }
