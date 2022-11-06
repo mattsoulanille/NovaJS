350e48a8efc4e50b83ccbe91219d11226fbaba48
Matthew Soulanille <matthew@soulanille.net>
2022-11-05 17:32:54 -0700

    Move 'currentIfDraft' out of nova_ecs

--- nova/src/util/deimmerify.ts
+++ nova/src/util/deimmerify.ts
@@ -1,5 +1,12 @@
 import { Entity } from "nova_ecs/entity";
-import { currentIfDraft } from "nova_ecs/utils";
+import { current, Draft, isDraft } from "immer";
+
+export function currentIfDraft<T>(val: T | Draft<T>): T {
+    if (isDraft(val)) {
+        return current(val) as T;
+    }
+    return val as T;
+}
 
 export function deImmerify(entity: Entity) {
     for (const [component, value] of entity.components) {
