8bbb836bbf9748b7e97da8eab6c4c40f22f6bc8b
Matthew Soulanille <matthew@soulanille.net>
2022-11-05 17:15:41 -0700

    Split ProvideAsync into a different file

---
+++ nova_ecs/async_provider.ts
@@ -0,0 +1,114 @@
+import { isDraft, original } from "immer";
+import { ArgsToData, ArgTypes, GetEntity, UUID } from "./arg_types";
+import { AsyncSystem } from "./async_system";
+import { Component } from "./component";
+import { DeleteEvent, StepEvent } from "./events";
+import { Optional } from "./optional";
+import { ChangeEvents, ProvidePlugin, Without } from "./provider";
+import { Resource } from "./resource";
+import { System, SystemArgs } from "./system";
+import { Plugin } from "./plugin";
+
+type AsyncProviderData =
+    Map<string /* provider system name */,
+        Map<string /* entity uuid */,
+            Symbol | undefined /* running */>>;
+
+export const AsyncProviderResource =
+    new Resource<AsyncProviderData>('AsyncProviderResource');
+
+export type ProvideAsyncArgs<Data, Args extends readonly ArgTypes[]> =
+    Without<SystemArgs<Args>, 'step' | 'events'> & {
+        provided: Component<Data>,
+        factory: (...args: ArgsToData<Args>) => Data | Promise<Data>,
+        update?: Iterable<Component<any>>,
+        onError?: (err: Error) => void,
+    };
+
+export function ProvideAsync<Data, Args extends readonly ArgTypes[]>({ name, provided, update, factory, args, before, after, onError }: ProvideAsyncArgs<Data, Args>) {
+    const updateEvents = [...update ?? []].map(component => ChangeEvents.get(component));
+
+    return new AsyncSystem({
+        name,
+        events: [StepEvent, ...updateEvents],
+        before, after,
+        args: [Optional(provided), AsyncProviderResource, UUID,
+            GetEntity, Optional(StepEvent), ...args] as const,
+        skipIfApplyingPatches: true,
+        exclusive: true,
+        async step(providedValue, asyncProviderData, uuid, entity, step, ...args) {
+            const originalProvidedValue = entity.components.get(provided);
+
+            const originalData = originalIfDraft(asyncProviderData);
+            const running = originalData.get(name)?.get(uuid);
+            if ((running || providedValue !== undefined) && step) {
+                return;
+            }
+
+            // Since all arguments are drafted, this must be set on the original entity.
+            // Otherwise, it will only actually be set once this function completes.
+            const runningSymbol = Symbol(); // Unique to this run
+
+            if (!originalData.has(name)) {
+                originalData.set(name, new Map());
+            }
+            // Set this on the original data so no new instances
+            // of this provider will run on `step` events. Otherwise,
+            // it only applies after this instance finishes.
+            const originalDataForProvider = originalData.get(name)!;
+            originalDataForProvider.set(uuid, runningSymbol);
+            try {
+                providedValue = await factory(...args);
+
+                // If this instance of the provider is the most recent run,
+                // then apply the provided data.
+                if (originalDataForProvider.get(uuid) === runningSymbol) {
+
+                    // If the provided value is changed while the provider is running,
+                    // don't set the value.
+                    // TODO: Is this desirable?
+                    if (entity.components.get(provided) !== originalProvidedValue) {
+                        return;
+                    }
+                    entity.components.set(
+                        provided, originalIfDraft(providedValue));
+                }
+            } catch (e) {
+                (onError ?? console.warn)(e);
+            } finally {
+                if (originalDataForProvider.get(uuid) === runningSymbol) {
+                    // Set this on the draft so it gets applied after the async system
+                    // runs and applies the changes that were made.
+                    asyncProviderData.get(name)?.delete(uuid);
+                }
+            }
+        }
+    });
+}
+
+export function originalIfDraft<T>(val: T): T {
+    if (isDraft(val)) {
+        return original(val)!;
+    }
+    return val;
+}
+
+const AsyncProviderCleanup = new System({
+    name: 'AsyncProviderCleanup',
+    events: [DeleteEvent],
+    args: [UUID, AsyncProviderResource] as const,
+    step(deleted, asyncProviderData) {
+        for (const entities of asyncProviderData.values()) {
+            entities.delete(deleted);
+        }
+    }
+});
+
+export const ProvideAsyncPlugin: Plugin = {
+    name: 'ProvidePlugin',
+    build: (world) => {
+        world.addPlugin(ProvidePlugin); // for component change events.
+        world.resources.set(AsyncProviderResource, new Map());
+        world.addSystem(AsyncProviderCleanup);
+    }
+}
--- nova_ecs/provider.ts
+++ nova_ecs/provider.ts
@@ -1,8 +1,7 @@
-import { isDraft, original } from "immer";
-import { ArgsToData, ArgTypes, GetEntity, UUID } from "./arg_types";
-import { AsyncSystem } from "./async_system";
+import { ArgsToData, ArgTypes, GetEntity } from "./arg_types";
 import { Component } from "./component";
-import { DeleteEvent, EcsEvent, StepEvent } from "./events";
+import { EcsEvent, StepEvent } from "./events";
+import { SyncSubscription } from "./event_map";
 import { Optional } from "./optional";
 import { Plugin } from "./plugin";
 import { Resource } from "./resource";
@@ -12,7 +11,7 @@ import { DefaultMap } from "./utils";
 export const ChangeEvents = new DefaultMap<Component<any>, EcsEvent<true>>(
     component => new EcsEvent(`${component.name} changed`));
 
-type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
+export type Without<T, K> = Pick<T, Exclude<keyof T, K>>;
 type ProvideArgs<Data, Args extends readonly ArgTypes[]> =
     Without<SystemArgs<Args>, 'step' | 'events'> & {
         provided: Component<Data>,
@@ -42,110 +41,25 @@ export function Provide<Data, Args extends readonly ArgTypes[]>({ name, provided
     });
 }
 
-type AsyncProviderData =
-    Map<string /* provider system name */,
-        Map<string /* entity uuid */,
-            Symbol | undefined /* running */>>;
-
-const AsyncProviderResource =
-    new Resource<AsyncProviderData>('AsyncProviderResource');
-
-type ProvideAsyncArgs<Data, Args extends readonly ArgTypes[]> =
-    Without<SystemArgs<Args>, 'step' | 'events'> & {
-        provided: Component<Data>,
-        factory: (...args: ArgsToData<Args>) => Data | Promise<Data>,
-        update?: Iterable<Component<any>>,
-        onError?: (err: Error) => void,
-    };
-
-export function ProvideAsync<Data, Args extends readonly ArgTypes[]>({ name, provided, update, factory, args, before, after, onError }: ProvideAsyncArgs<Data, Args>) {
-    const updateEvents = [...update ?? []].map(component => ChangeEvents.get(component));
-
-    return new AsyncSystem({
-        name,
-        events: [StepEvent, ...updateEvents],
-        before, after,
-        args: [Optional(provided), AsyncProviderResource, UUID,
-            GetEntity, Optional(StepEvent), ...args] as const,
-        skipIfApplyingPatches: true,
-        exclusive: true,
-        async step(providedValue, asyncProviderData, uuid, entity, step, ...args) {
-            const originalProvidedValue = entity.components.get(provided);
-
-            const originalData = originalIfDraft(asyncProviderData);
-            const running = originalData.get(name)?.get(uuid);
-            if ((running || providedValue !== undefined) && step) {
-                return;
-            }
-
-            // Since all arguments are drafted, this must be set on the original entity.
-            // Otherwise, it will only actually be set once this function completes.
-            const runningSymbol = Symbol(); // Unique to this run
-
-            if (!originalData.has(name)) {
-                originalData.set(name, new Map());
-            }
-            // Set this on the original data so no new instances
-            // of this provider will run on `step` events. Otherwise,
-            // it only applies after this instance finishes.
-            const originalDataForProvider = originalData.get(name)!;
-            originalDataForProvider.set(uuid, runningSymbol);
-            try {
-                providedValue = await factory(...args);
-
-                // If this instance of the provider is the most recent run,
-                // then apply the provided data.
-                if (originalDataForProvider.get(uuid) === runningSymbol) {
-
-                    // If the provided value is changed while the provider is running,
-                    // don't set the value.
-                    // TODO: Is this desirable?
-                    if (entity.components.get(provided) !== originalProvidedValue) {
-                        return;
-                    }
-                    entity.components.set(
-                        provided, originalIfDraft(providedValue));
-                }
-            } catch (e) {
-                (onError ?? console.warn)(e);
-            } finally {
-                if (originalDataForProvider.get(uuid) === runningSymbol) {
-                    // Set this on the draft so it gets applied after the async system
-                    // runs and applies the changes that were made.
-                    asyncProviderData.get(name)?.delete(uuid);
-                }
-            }
-        }
-    });
-}
-
-function originalIfDraft<T>(val: T): T {
-    if (isDraft(val)) {
-        return original(val)!;
-    }
-    return val;
-}
-
-const AsyncProviderCleanup = new System({
-    name: 'AsyncProviderCleanup',
-    events: [DeleteEvent],
-    args: [UUID, AsyncProviderResource] as const,
-    step(deleted, asyncProviderData) {
-        for (const entities of asyncProviderData.values()) {
-            entities.delete(deleted);
-        }
-    }
-});
+// Keeps track of whether entity change events are being re-emitted into
+// the world.
+// TODO: Should this be moved to World and always happen?
+const ChangeEventsSubscription =
+    new Resource<SyncSubscription>('ChangeEventsSubscription');
 
 export const ProvidePlugin: Plugin = {
     name: 'ProvidePlugin',
     build: (world) => {
-        world.resources.set(AsyncProviderResource, new Map());
-        world.addSystem(AsyncProviderCleanup);
-        world.entities.events.changeComponent.subscribe(
-            ([uuid, _entity, component]) => {
-                world.emit(ChangeEvents.get(component), true, [uuid]);
-            });
-
+        // Subscribe to change events of components on entities.
+        // TODO: This change detection only detects when a component is
+        // reassigned, not when it's modified (no deep change detection).
+        // Is this useful in its current state?
+        if (!world.resources.has(ChangeEventsSubscription)) {
+            const unsubscribe = world.entities.events.changeComponent.subscribe(
+                ([uuid, _entity, component]) => {
+                    world.emit(ChangeEvents.get(component), true, [uuid]);
+                });
+            world.resources.set(ChangeEventsSubscription, unsubscribe);
+        }
     }
 }
